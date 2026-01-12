5a1caf40df942741f5faec2055b467794a55fdc2
Dmitry <deimos715@gmail.com>
2026-01-12 16:47:20 +0300

    lesson validation

--- backend/src/book/serializers.py
+++ backend/src/book/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
+from .models import Book
 
 class BookSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200, default='Django REST Framework')
     author = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
